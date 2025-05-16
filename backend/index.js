ea471d0e115b08748a3cca0029ab64f9a80af610
Forever Git <amehra.mehra@gmail.com>
2025-05-16 14:50:59 +0530

    Commit Changes Astha

--- backend/index.js
+++ backend/index.js
@@ -76,11 +76,11 @@ app.get("/vivid_sitepeople_assoc", (req, res) => {
     const { firstname, lastname, email, phone, loginid,password } = req.body;
   
     const query = `
-      INSERT INTO vivid_people (firstname, lastname, email, contact, loginid,password,documenturl)
-      VALUES (?, ?, ?, ?, ?,?,?)
+      INSERT INTO vivid_people (firstname, lastname, email, contact, loginid,password,documenturl,profile_type)
+      VALUES (?, ?, ?, ?, ?,?,?,?)
     `;
   
-    db.query(query, [firstname, lastname, email, phone, loginid,password,'https://plus.unsplash.com/premium_photo-1689568158814-3b8e9c1a9618?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cGVyc29uYXxlbnwwfHwwfHx8MA%3D%3D'], (err, result) => {
+    db.query(query, [firstname, lastname, email, phone, loginid,password,'https://plus.unsplash.com/premium_photo-1689568158814-3b8e9c1a9618?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cGVyc29uYXxlbnwwfHwwfHx8MA%3D%3D','Cleaner'], (err, result) => {
       if (err) return res.status(500).json({ error: err.message });
       res.json({ message: "Cleaner added successfully" });
     });
